d91d192a5d480e5effb80cee29db390e5273f213
Jenny Kim <kimj1905@gmail.com>
2021-07-02 14:29:41 -0400

    commit tree 160843a40358dbf1c7e4e77277553ad3514dac00

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,30 @@
+class SchoolClassesController < ApplicationController
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(params_sub)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params_sub)
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+  def params_sub
+    params.require(:school_class).permit(:title, :room_number)
+  end
+end
